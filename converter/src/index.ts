4b37542209449365c3e0251211cc0e549f4b539b
Roman Nosyrev <romanser91@gmail.com>
2022-04-02 19:43:46 +0300

    add server and endpoint

--- converter/src/index.ts
+++ converter/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!!!')
+import express from "express";
+
+const app = express();
+// TODO: pass port from env
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
