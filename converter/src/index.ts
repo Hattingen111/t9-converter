6d624bd38c59f5adc1b3977eef7f7c3f3a36f46f
Roman Nosyrev <romanser91@gmail.com>
2022-04-03 23:56:17 +0300

    fix cors usage

--- converter/src/index.ts
+++ converter/src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import { getRouter } from "./router";
 import { setupWordDict } from "./setupWordDict";
@@ -8,6 +9,7 @@ const port = 8000;
 
 const trie = setupWordDict();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", getRouter(trie));
 
